3059f2f88b1d46740bd8e7506edd45aa5b0e582e
Rebecca Martin <becki_213@hotmail.com>
2024-03-04 15:01:06 +0000

    Empty answer-box after each answer has been submitted

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,8 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+    //Make answer-box empty after each answer submittion
+    document.getElementById("answer-box").value = "";
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
